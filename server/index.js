a8733e2d00d8a1dd469a1d8abcb73090a80c7e16
Avi <avimehta9@gmail.com>
2022-06-11 20:53:27 -0400

    mainframe done

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const colors = require('colors');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
+const cors = require('cors')
 const schema = require('./schema/schema')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000;
@@ -9,6 +10,8 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 connectDB();
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema,
